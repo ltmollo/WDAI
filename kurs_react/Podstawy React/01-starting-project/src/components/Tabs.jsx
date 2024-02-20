8fc7093d7ffd66bc5dbfe75a05e24f40c31d80e6
ltmollo <ltmollo@wp.pl>
2024-02-20 09:15:53 +0100

    First Push

---
+++ kurs_react/Podstawy React/01-starting-project/src/components/Tabs.jsx
@@ -0,0 +1,13 @@
+export default function Tabs({children, buttons, buttonContainer}){
+    const ButtonsContainer = buttonContainer;
+
+    return (
+        <>
+            <ButtonsContainer>
+                {buttons}
+            </ButtonsContainer>
+            {children}
+        </>
+
+    )
+}
